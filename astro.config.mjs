04c28eccd76b813778426e2120e9f17d865e75ac
André Lima <me@limwa.pt>
2023-11-24 02:22:21 +0000

    chore: change site to www.limwa.pt

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://limwa.pt",
+  site: "https://www.limwa.pt",
 
   integrations: [solidJs(), tailwind()],
 
