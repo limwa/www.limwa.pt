1ecebff1016a64c4bce50402b7d94675b426a092
André Lima <me@limwa.pt>
2023-11-24 01:37:37 +0000

    feat: initial preview version

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig, passthroughImageService } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://limwa.pt",
+
+  integrations: [solidJs(), tailwind()],
+
+  output: "hybrid",
+  adapter: cloudflare(),
+
+  image: {
+    service: passthroughImageService(),
+  },
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	// theme: {
+	// 	extend: {
+	// 		keyframes: {
+	// 			pulse: {
+	// 				"0%, 100%": {
+	// 					opacity: '1', 
+	// 				},
+	// 				"50%": {
+	// 					opacity: '0.6',
+	// 				}
+	// 			}
+	// 		},
+	// 		animation: {
+	// 			pulse: "pulse 5s ease-in-out infinite"
+	// 		}
+	// 	},
+	// },
+	plugins: [],
+}
