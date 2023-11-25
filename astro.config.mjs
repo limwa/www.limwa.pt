f2e76eea609beea66a98a7d3c060be6e1ffea84d
André Lima <me@limwa.pt>
2023-11-25 20:46:31 +0000

    feat: add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
+
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.limwa.pt",
 
-  integrations: [solidJs(), tailwind()],
-
+  integrations: [
+    solidJs(),
+    tailwind(),
+    robotsTxt(),
+    sitemap()
+  ],
+  
   output: "hybrid",
   adapter: cloudflare(),
 
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 });
