06b4809d954888348a7019614d24eafc3f7943d4
André Lima <me@limwa.pt>
2024-01-16 03:31:38 +0000

    feat: color mix slate shades for selection color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: ({ colors }) => {
+                return {
+					...colors,
+					slate: {
+						...colors.slate,
+						"750": "#283548", // color-mix(in oklab, ${colors.slate["700"]}, ${colors.slate["800"]})
+					}
+				};
+			}
 	// 		keyframes: {
 	// 			pulse: {
 	// 				"0%, 100%": {
